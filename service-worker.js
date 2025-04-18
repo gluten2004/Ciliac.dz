ba0021dc7f261b9e94825fd92a2d049edcda533a
gluten2004 <glutenfrecilaic.dz@gmail.com>
2025-04-18 23:52:04 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open('ciliac-cache').then(function(cache) {
       return cache.addAll([
-        '/', // تأكد من أن المسار إلى الصفحة الرئيسية صحيح
+        '/Ciliac.dz/',
         '/Ciliac.dz/index.html',
         '/Ciliac.dz/manifest.json',
         '/Ciliac.dz/icons/icon-192.png',
@@ -10,9 +10,9 @@ self.addEventListener('install', function(e) {
         '/Ciliac.dz/doctors.html',
         '/Ciliac.dz/recipes.html',
         '/Ciliac.dz/maps.html'
-      ]).catch(function(error) {
-        console.error('فشل في إضافة الملفات إلى الكاش:', error);
-      });
+      ]);
+    }).catch(function(error) {
+      console.error('فشل في إضافة الملفات إلى الكاش:', error);
     })
   );
 });
@@ -20,9 +20,7 @@ self.addEventListener('install', function(e) {
 self.addEventListener('fetch', function(e) {
   e.respondWith(
     caches.match(e.request).then(function(response) {
-      // إذا كانت هناك استجابة من الكاش، ارجع إليها، وإلا قم بتحميلها من الإنترنت
       return response || fetch(e.request).then(function(networkResponse) {
-        // يمكن إضافة الكاش للملفات الجديدة بعد تحميلها من الإنترنت
         if (networkResponse && networkResponse.status === 200) {
           caches.open('ciliac-cache').then(function(cache) {
             cache.put(e.request, networkResponse.clone());
